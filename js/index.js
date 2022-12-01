5175e91a6bfdfa9af5a419f10a460638e08a6353
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-12-01 22:21:37 +0400

    Добавляет рендер товаров первой вкладки при загрузке страницы

--- js/index.js
+++ js/index.js
@@ -24,48 +24,65 @@ mediaQueryList.addEventListener('change', (evt) => {
 const categoryContent = document.querySelector('.catalog__categories');
 
 // Рендер категорий
-const categoryList = () => {
+const renderCategoryList = () => {
     const categoryItem = categories.map((value) => {
+        let active = (value.categoryId === 1) ? 'catalog__btn--is-active' : '';
+
         return `
             <li class="catalog__categories-item">
-                <button type="button" class="catalog__btn" data-id="${value.categoryId}" title="${value.categoryName}">${value.categoryName}</button>
+                <button type="button" class="catalog__btn js-tabBtn ${active}" data-id="${value.categoryId}" title="${value.categoryName}">${value.categoryName}</button>
             </li>
         `;
     }).join('');
 
     return categoryItem;
-}
+};
 
-categoryContent.innerHTML = categoryList();
+categoryContent.innerHTML = renderCategoryList();
 
 const catalogContent = document.querySelector('.catalog__content');
-const categoryTabs = document.querySelectorAll('.catalog__categories-item');
+const categoryTabs = document.querySelectorAll('.js-tabBtn');
 
-// Рендер товаров при нажатии на категорию
+// Рендер списка товаров
+const renderProductList = (id) => {
+    let productList = '';
+
+    products.forEach((product) => {
+        const createProduct = () => {
+            const productItem = `
+                <li class="product">
+                    <img src="img/tovar.jpg" alt="${product.productName}">
+                    <span>${product.productName}</span>
+                </li>
+            `;
+
+            return productItem;
+        };
+
+        if (product.categoryId === Number(id)) {
+            productList = productList + createProduct();
+        }
+
+        return;
+    })
+
+    catalogContent.innerHTML = productList;
+};
+
 categoryTabs.forEach((tab) => {
+    // Рендер товаров при загрузке страницы в начальной категории
+    const initialProductId = tab.dataset?.id;
+    renderProductList(initialProductId);
+
+    // Рендер товаров при нажатии на категорию
     tab.addEventListener('click', (evt) => {
+        const activeTab = document.querySelector('.catalog__btn.catalog__btn--is-active');
+        const currentTab = evt.target;
+
+        activeTab?.classList.remove('catalog__btn--is-active');
+        currentTab?.classList.add('catalog__btn--is-active');
+
         const productId = evt.target.dataset.id;
-        let productList = '';
-
-        products.forEach((product) => {
-            const createProduct = () => {
-                const productItem = `
-                    <li class="product">
-                        <img src="img/tovar.jpg" alt="${product.productName}">
-                        <span>${product.productName}</span>
-                    </li>
-                `;
-
-                return productItem;
-            };
-
-            if (product.categoryId === Number(productId)) {
-                productList = productList + createProduct();
-            }
-
-            return;
-        })
-
-        catalogContent.innerHTML = productList;
+        renderProductList(productId);
     })
 });
