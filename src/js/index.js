9e400ade9d79135a1019fa10f65c31223d81307e
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-12-01 14:37:31 +0400

    Добавляет динамический рендер товаров по категориям

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,54 @@ mediaQueryList.addEventListener('change', (evt) => {
         tab?.classList.remove('catalog__container--is-visible');
     }
 });
+
+// Рендер категорий и товаров
+
+const categoryContent = document.querySelector('.catalog__categories');
+
+// Рендер категорий
+const categoryList = () => {
+    const categoryItem = categories.map((value) => {
+        return `
+            <li class="catalog__categories-item">
+                <button type="button" class="catalog__btn" data-id="${value.categoryId}" title="${value.categoryName}">${value.categoryName}</button>
+            </li>
+        `;
+    }).join('');
+
+    return categoryItem;
+}
+
+categoryContent.innerHTML = categoryList();
+
+const catalogContent = document.querySelector('.catalog__content');
+const categoryTabs = document.querySelectorAll('.catalog__categories-item');
+
+// Рендер товаров при нажатии на категорию
+categoryTabs.forEach((tab) => {
+    tab.addEventListener('click', (evt) => {
+        const productId = evt.target.dataset.id;
+        let productList = '';
+
+        products.forEach((product) => {
+            const createProduct = () => {
+                const productItem = `
+                    <li class="product">
+                        <img src="/src/img/tovar.jpg" alt="${product.productName}">
+                        <span>${product.productName}</span>
+                    </li>
+                `;
+
+                return productItem;
+            };
+
+            if (product.categoryId === Number(productId)) {
+                productList = productList + createProduct();
+            }
+
+            return;
+        })
+
+        catalogContent.innerHTML = productList;
+    })
+});
